d89d7ccc2d0ce8d02e1f1e1386c3c08c3b447c10
Dreezy305 <bankoleidris@gmail.com>
2021-09-12 22:46:22 +0000

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Blog from "./components/Blog";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" render={(props) => <Home {...props} />} />
+
+          <Route exact path="/about" render={(props) => <About {...props} />} />
+
+          <Route exact path="/blog" render={(props) => <Blog {...props} />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,15 @@
 import React from "react";
-
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
 import MenuIcon from "@material-ui/icons/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { withRouter } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,29 +19,98 @@ const useStyles = makeStyles((theme) => ({
     marginRight: theme.spacing(2),
   },
   title: {
-    flexGrow: 1,
+    [theme.breakpoints.down("sm")]: {
+      flexGrow: 1,
+    },
   },
 }));
 
-export default function NavBar() {
+function NavBar(props) {
+  const { history } = props;
+  console.log(history);
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClick = (pageURL) => {
+    history.push(pageURL);
+    setAnchorEl(null);
+  };
+
+  const menuList = [
+    "HOME",
+    "ABOUT",
+    "PATIENT PORTAL",
+    "SERVICES",
+    "BLOG",
+    "CONTACT",
+  ];
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar display="inline">
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          ></IconButton>
-          <Typography edge="start" variant="h6" className="">
-            Dokita
+      <AppBar position="static" color="transparent">
+        <Toolbar disableGutter>
+          <Typography variant="h6" className={classes.title}>
+            Photos
           </Typography>
-          <Button color="inherit">Login</Button>
+
+          <div>
+            {isMobile ? (
+              <>
+                <IconButton
+                  edge="start"
+                  className={classes.menuButton}
+                  color="inherit"
+                  aria-label="menu"
+                  onClick={handleMenu}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={open}
+                  onClose={() => setAnchorEl(null)}
+                >
+                  <MenuItem onClick={() => handleMenuClick("/")}>Home</MenuItem>
+                  <MenuItem onClick={() => handleMenuClick("/about")}>
+                    About Us
+                  </MenuItem>
+                  <MenuItem onClick={() => handleMenuClick("/blog")}>
+                    Blog
+                  </MenuItem>
+                </Menu>
+              </>
+            ) : (
+              <div>
+                <Button variant="contained">HOME</Button>
+                <Button variant="contained">ABOUT US</Button>
+                {/*<Button>PATIENT PORTAL</Button>*/}
+                {/*<Button>SERVICES</Button>*/}
+                <Button variant="contained">BLOG</Button>
+                {/*<Button>CONTACT</Button>*/}
+              </div>
+            )}
+          </div>
         </Toolbar>
       </AppBar>
     </div>
   );
 }
+
+export default withRouter(NavBar);
