048ea02054693a463f0c759b9b916f15f238302c
Dreezy305 <bankoleidris@gmail.com>
2021-09-24 15:06:48 +0000

    dashboard layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/navbar";
@@ -10,6 +10,7 @@ import Contact from "./components/contact";
 import Services from "./components/services";
 import Login from "./auth/login";
 import SignUp from "./auth/signup";
+import Layout from "./dashboard/layout";
 
 function App() {
   const RoutesList = [
@@ -19,25 +20,28 @@ function App() {
     { id: 4, Component: Services, path: "/services", title: "Services" },
     { id: 5, Component: Blog, path: "/blog", title: "Blog" },
     { id: 6, Component: Contact, path: "/contact", title: "Contact" },
-    { id: 7, Component: Login, path: "/login", title: "Login" },
-    { id: 8, Component: SignUp, path: "/signup", title: "SignUp" },
   ];
 
   return (
     <div className="App">
       <Router>
-        <NavBar />
         <Switch>
-          {RoutesList.map((Routes) => (
-            <Route
-              exact
-              key={Routes.id}
-              path={Routes.path}
-              render={(props) => {
-                return <Routes.Component {...props} />;
-              }}
-            />
-          ))}
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/dashboard" component={Layout} />
+          <div>
+            <NavBar />
+            {RoutesList.map((Routes) => (
+              <Route
+                exact
+                key={Routes.id}
+                path={Routes.path}
+                render={(props) => {
+                  return <Routes.Component {...props} />;
+                }}
+              />
+            ))}
+          </div>
         </Switch>
       </Router>
     </div>
---
+++ src/dashboard/layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Layout() {
+  return (
+    <div>
+      <h1>Dashboard is coming soon</h1>
+    </div>
+  );
+}
+
+export default Layout;
