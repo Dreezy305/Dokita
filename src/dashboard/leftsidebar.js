29a02820579136ba51f4b05946a782e4709c3db7
Dreezy305 <bankoleidris@gmail.com>
2021-09-24 17:18:49 +0000

    right, left and top bars

--- src/dashboard/leftsidebar.js
+++ src/dashboard/leftsidebar.js
@@ -1,4 +1,9 @@
 import React from "react";
+import Dashboard from "../assets/icons/dashboard.svg";
+import Doctors from "../assets/icons/doctors.svg";
+import Appointment from "../assets/icons/appointment.svg";
+import Setting from "../assets/icons/setting.svg";
+import Logout from "../assets/icons/logout.svg";
 
 function LeftSideBar() {
   const SideBarSection = () => {};
